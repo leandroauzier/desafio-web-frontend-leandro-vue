33c0ff11f70fd338b068d3c1ddc1c830c5c1015d
leandro <leandroauzier02@gmail.com>
2021-10-14 22:04:06 -0300

    Realizando testes com Vue

---
+++ src/components/MeuComponent.vue
@@ -0,0 +1,19 @@
+<!-- Template só aceita um unico tipo de arquivo, para mais de um, botar numa unica DIV -->
+<template>
+  <div>
+    <h1>Ola Vue!</h1>
+    <p>Meu nome é {{nome}} e sou {{profissao}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MeuComponent",
+    data(){
+        return{
+          nome:"Leandro",
+          profissao:"Programador"
+        }      
+      }
+  }
+</script>
